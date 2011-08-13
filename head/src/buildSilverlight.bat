a501494862a995062b7eda667a4a98d14b840c3a
Dan Mohl <dmohl@yahoo.com>
2011-08-13 07:27:41 -0500

    updated the buildSilverlight.bat

--- head/src/buildSilverlight.bat
+++ head/src/buildSilverlight.bat
@@ -1,16 +1,16 @@
 
 cd FSharp.PowerPack
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight /p:SilverlightVersion=2.0 %1 %2 %3 %4 %5 %6 %7 %8 %9
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight /p:SilverlightVersion=v5.0 %1 %2 %3 %4 %5 %6 %7 %8 %9
 cd ..
 
 cd FSharp.PowerPack.Compatibility
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight %1 %2 %3 %4 %5 %6 %7 %8 %9
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight /p:SilverlightVersion=v5.0 %1 %2 %3 %4 %5 %6 %7 %8 %9
 cd ..
 
 cd FSharp.PowerPack.Linq
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight %1 %2 %3 %4 %5 %6 %7 %8 %9
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight /p:SilverlightVersion=v5.0 %1 %2 %3 %4 %5 %6 %7 %8 %9
 cd ..
 
 cd FSharp.PowerPack.Unittests
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight %1 %2 %3 %4 %5 %6 %7 %8 %9
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild /p:TargetFramework=Silverlight /p:SilverlightVersion=v5.0 %1 %2 %3 %4 %5 %6 %7 %8 %9
 cd ..
